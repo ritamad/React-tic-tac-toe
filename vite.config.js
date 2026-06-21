1431fd89b627ae7b32fb1ea3486d972d8d7c625d
Rita Madio <160418348+ritamad@users.noreply.github.com>
2026-06-21 10:15:18 +0200

    fix: delete unnecessary comment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React-tic-tac-toe/", // <-- AGGIUNGI QUESTA RIGA ESATTA
+  base: "/React-tic-tac-toe/",
 });
